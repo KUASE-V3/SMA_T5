8aa8f573df95495c185e127c18021b8e755df334
Sadturtleman <rugsn12345@gmail.com>
2025-05-09 03:28:28 +0000

    test

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,11 @@ int main() {
     Person p("Alice", 30);
     std::cout << p.getName() << " is " << p.getAge() << " years old." << std::endl;
     Person p("Alice", 40);
-    std::cout << p.getName() << " is " << p.getAge() << " years old." << std::endl;
+    std::cout << p.getName() << " issdf " << p.getAge() << " years old." << std::endl;
+    Person p("Alice", 40);
+    std::cout << p.getName() << " issdf " << p.getAge() << " years old." << std::endl;
+    Person p("Alice", 40);
+    std::cout << p.getName() << " issdf " << p.getAge() << " years old." << std::endl;
     
     return 0;
 }
