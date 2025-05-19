6b7609cf7f605e02032800a3e366a9bce35f3b43
ChilledPi <chilledpi7@gmail.com>
2025-05-19 02:42:33 +0000

    fix: 선결제 거부시 좌표 출력하도록 수정

--- src/domain/CLManager.cpp
+++ src/domain/CLManager.cpp
@@ -74,11 +74,23 @@ void CLManager::run() {
                     }
                 }
             } else if (status == ORDER_STATUS::REMOTE) {
-              auto result = prePay(payment);
-              if (result.has_value()) {
-                const Dvm &dvm = result->get();
+              std::cout << "선결제 하시겠습니까?" << endl
+                        << "1. 네" << endl
+                        << "2. 아니오" << endl;
+              int select = readInt("입력:");
+              if(select == 1) {
+                auto result = prePay(payment);
+                if (result.has_value()) {
+                  const Dvm &dvm = result->get();
+                } else {
+                  // 선결제 실패 재고 없어서 or 통신 실패
+                }
+              } else if(select == 2) {
+                std::cout << "가장 가까운 자판기 좌표는 ("
+                          << dvmNavigator->begin()->x << ", "
+                          << dvmNavigator->begin()->y << endl;
               } else {
-                // 선결제 실패 재고 없어서 or 통신 실패
+                std::cout << invalidMenuMsg << std::endl;
               }
             } else if (status == ORDER_STATUS::FAIL) {
                 std::cout << orderFailMsg;
