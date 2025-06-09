4ef7b5a8362e8d126261ef66279b693eaae7252c
mojunsul <sdg2435@naver.com>
2025-06-09 12:18:19 +0900

    CLManager 리팩토링 Cognitive Complexity

--- include/domain/CLManager.h
+++ include/domain/CLManager.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "CertificationCodeFactory.h"
 #include "Dvm.h"
 #include "ItemManager.h"
@@ -6,6 +7,8 @@
 #include "NetworkManager.h"
 #include "Payment.h"
 #include "PrepaymentStock.h"
+#include <memory>
+#include <optional>
 #include <set>
 #include <string>
 
@@ -13,6 +16,7 @@ enum class ORDER_STATUS { LOCAL, REMOTE, FAIL };
 
 class CLManager {
   private:
+    // Singleton dependencies
     ItemManager *itemManager;
     PrepaymentStock *prepaymentStock;
     MessageFactory *messageFactory;
@@ -21,22 +25,34 @@ class CLManager {
 
     std::unique_ptr<std::set<Dvm>> dvmNavigator;
 
+    // Input
     int readInt(std::string text);
     std::string readString(std::string text);
+    int promptValidItemCode();
+    int promptValidQuantity(int itemCode);
 
-    // 싱글톤
+    // Flow control
+    void handleMenu(int select);
+    void processOrder();
+    void checkPrepaymentCode();
+
+    // Order handling
+    void handleLocalPayment(std::unique_ptr<Payment> payment);
+    void handleRemotePayment(std::unique_ptr<Payment> payment);
+
+    // Singleton enforcement
     CLManager();
     CLManager(const CLManager &) = delete;
     CLManager &operator=(const CLManager &) = delete;
 
   public:
-    // 싱글톤 인스턴스
     static CLManager &getInstance();
+
     void run();
     void showItems();
-    // CLManager.h
+
     ORDER_STATUS order(std::unique_ptr<Payment> &payment);
     bool pay(std::unique_ptr<Payment> &payment);
     std::optional<std::reference_wrapper<const Dvm>> prePay(std::unique_ptr<Payment> &payment);
-    optional<Payment> enterCertCode(string certCode);
+    std::optional<Payment> enterCertCode(std::string certCode);
 };
